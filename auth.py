bc8f3518b91871edd43f8a8e7874e4cd047753d5
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-05-19 22:28:41 +0300

    Add message logging mechanism

--- auth.py
+++ auth.py
@@ -1,9 +1,26 @@
 from flask import Flask, request, Response
 from json import dumps
 import hashlib
-from datetime import datetime, timedelta
-
+from datetime import datetime
 import authUtils
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
@@ -16,11 +33,13 @@ def register():
     # Get hashed secret using SHA256
     hashed_secret = hashlib.sha256(bytes(client_secret, 'utf-8')).hexdigest()
 
-    registerResult = authUtils.register(client_id, hashed_secret, False) # TODO allow admins to add admins on a different route
+    registerResult = authUtils.register(client_id, hashed_secret, False, app.logger) # TODO allow admins to add admins on a different route
     
     if registerResult:
+        app.logger.info(f"User {client_id} registered into the system.")
         return Response(status=201)
     
+    app.logger.error(f"User {client_id} could not be registered.")
     return Response(status=400)
 
 # Route for authentication
@@ -32,11 +51,13 @@ def login():
     # Get hashed secret using SHA256
     hashed_secret = hashlib.sha256(bytes(client_secret, 'utf-8')).hexdigest()
 
-    authResult = authUtils.authenticate(client_id, hashed_secret)
+    authResult = authUtils.authenticate(client_id, hashed_secret, app.logger)
 
     if authResult == False:
+        app.logger.error(f"User {client_id} was not authenticated.")
         return Response(status=401)
     else: 
+        app.logger.info(f"User {client_id} authenticated succesfully.")
         return Response(dumps(authResult), status=200, mimetype='application/json')
 
 # Route for veryfing JWT
@@ -45,21 +66,26 @@ def verify():
     # Get the authorization header
     authorizationHeader = request.headers.get('authorization')
     if authorizationHeader is None:
+        app.logger.error("No token provided.")
         return Response(status=400)
     # Remove the 'Bearer ' keyword from authorization header	
     token = authorizationHeader[7:]
 
-    verifyResult = authUtils.validate(token)
+    verifyResult = authUtils.validate(token, app.logger)
 
     if verifyResult is None:
+        app.logger.error(f"Token {token} is invalid.")
         return Response(status=401)
     elif datetime.strptime(verifyResult.get('expirationTime'), "%m/%d/%Y, %H:%M:%S") < datetime.utcnow(): 
         # Invalidate token by adding to blacklist
-        invalidateResult = authUtils.invalidate(token)
+        app.logger.info(f"Token {token} expiration time was " + verifyResult.get('expirationTime'), "%m/%d/%Y, %H:%M:%S")
+        invalidateResult = authUtils.invalidate(token, app.logger)
 
-        if invalidateResult:
+        if not invalidateResult is None:
+            app.logger.error(f"Token {token} is no longer valid.")
             return Response(dumps({"error": "Token expired. Please log-in again."}), status=401, mimetype='application/json')
-        
+
+    app.logger.info(f"Token {token} is valid.")    
     return Response(dumps(verifyResult), status=200, mimetype='application/json')
 
 # Route for logging out
@@ -68,16 +94,19 @@ def logout():
     # Get the authorization header
     authorizationHeader = request.headers.get('authorization')
     if authorizationHeader is None:
+        app.logger.error("No token provided.")
         return Response(status=400)
     # Remove the 'Bearer ' keyword from authorization header	
     token = authorizationHeader[7:]
 
     # Invalidate token by adding to blacklist
-    invalidateResult = authUtils.invalidate(token)
+    invalidateResult = authUtils.invalidate(token, app.logger)
 
-    if invalidateResult:
+    if not invalidateResult is None:
+        app.logger.info(f"User {invalidateResult} logged out.")
         return Response(status=200)
     else:
+        app.logger.error(f"User could not be logged out.")
         return Response(status=400)
 
 
--- authUtils.py
+++ authUtils.py
@@ -2,7 +2,8 @@ import os
 import psycopg2
 import jwt
 from datetime import datetime, timedelta
-import sys
+from psycopg2.errorcodes import UNIQUE_VIOLATION
+from psycopg2 import errors
 
 # Get environment variables
 host = os.getenv('HOST_NAME')
@@ -13,22 +14,25 @@ db_pass_file = os.getenv('DB_PASSWORD_FILE')
 auth_secret_file = os.getenv('AUTH_SECRET_FILE')
 expire_time_seconds = int(os.getenv('EXPIRE_TIME_SEC'))
 
-def register(clientID, clientSecret, isAdmin):
+def register(clientID, clientSecret, isAdmin, logger):
     connection = None
 
     try:
         file = open(db_pass_file, "r")
         db_pass = file.read() 
         file.close()
+        logger.debug("Database password secret file was read.")
         
         connection = psycopg2.connect(host=host, dbname=auth_db_name, user=db_user, password=db_pass)
         cursor = connection.cursor()
-
+        logger.debug("Connection with database established.")
+        
         query = "INSERT INTO clients (\"ClientID\", \"ClientSecret\", \"IsAdmin\") VALUES (%s, %s, %s)"
 
         cursor.execute(query, (clientID, clientSecret, str(isAdmin)))
         connection.commit()
-        
+
+        logger.debug(f"User {clientID} inserted into the clients table.")
         cursor.close()
         connection.close()
 
@@ -45,23 +49,36 @@ def register(clientID, clientSecret, isAdmin):
                 CONSTRAINT {clientID}_unique UNIQUE (\"Name\"))"
 
         cursor.execute(create_query)
+        logger.debug(f"Table for {clientID}'s portfolio created.")
 
         # Add empty cash component to newly created table
         insert_query = f"INSERT INTO {clientID} (\"Name\", \"Quantity\") VALUES ('Cash', '0.0')"
         cursor.execute(insert_query)
+        logger.debug(f"Portfolio for {clientID} initialized with empty Cash component.")
 
         grant_query = f"GRANT ALL PRIVILEGES ON {clientID} TO portfolios_user"
         cursor.execute(grant_query)
-
+        logger.debug(f"Privileges on {clientID} to portfolios_user granted.")
         connection.commit()
 
         return True
     
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error, file=sys.stderr)
+    except errors.lookup(UNIQUE_VIOLATION) as error:
+        logger.error(f"User {clientID} already exists.")
+        logger.debug(error)
+        
         if connection is not None:
             cursor.close()
             connection.close()
+            logger.debug("Connection with database closed.")
+
+        return False       
+    except Exception as error:
+        logger.debug(error)
+        if connection is not None:
+            cursor.close()
+            connection.close()
+            logger.debug("Connection with database closed.")
 
         return False
 
@@ -69,44 +86,53 @@ def register(clientID, clientSecret, isAdmin):
         if connection is not None:
             cursor.close()
             connection.close()
+            logger.debug("Connection with database closed.")
 
-def authenticate(clientID, clientSecret):
+def authenticate(clientID, clientSecret, logger):
     connection = None
 
     try:
         file = open(db_pass_file, "r")
         db_pass = file.read() 
         file.close()
+        logger.debug("Database password secret file was read.")
 
         file = open(auth_secret_file, "r")
         auth_secret = file.read()
         file.close()
+        logger.debug("Auth secret file was read.")
 
         connection = psycopg2.connect(host=host, dbname=auth_db_name, user=db_user, password=db_pass)
         cursor = connection.cursor()
+        logger.debug("Connection with database established.")
+
         query = " SELECT * FROM clients WHERE \"ClientID\"='" + clientID + "' AND \"ClientSecret\"='" + clientSecret + "'"
         cursor.execute(query)
 
+        logger.debug("Select client query executed.")
         records = cursor.fetchall()
 
         if cursor.rowcount == 1:
             row = records[0]
-            now = datetime.now() + timedelta(seconds=expire_time_seconds)
+            exp_time = datetime.now() + timedelta(seconds=expire_time_seconds)
+            logger.debug("Expiration time calculated.")
             payload = {
                 "id": row[0],
                 "clientID": row[1],
-                "expirationTime": now.strftime("%m/%d/%Y, %H:%M:%S")
+                "expirationTime": exp_time.strftime("%m/%d/%Y, %H:%M:%S")
                 }
-            
+            logger.info(f"Credentials passed by {clientID} are valid.")
             token = jwt.encode(payload, auth_secret, algorithm='HS256')
-            
+            logger.debug("JWT Token created.")
+            logger.info("Created token with expiration time "+ exp_time.strftime("%m/%d/%Y, %H:%M:%S"))
             return {'token': token}
 
         else:
+            logger.error(f"Credentials for {clientID} are invalid.")
             return False
         
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        logger.debug(error)
         if connection is not None:
             cursor.close()
             connection.close()
@@ -118,37 +144,44 @@ def authenticate(clientID, clientSecret):
             cursor.close()
             connection.close()
 
-def validate(token):
+def validate(token, logger):
     connection = None
     try:
         file = open(db_pass_file, "r")
         db_pass = file.read() 
         file.close()
-        
+        logger.debug("Database password secret file was read.")
+
         file = open(auth_secret_file, "r")
         auth_secret = file.read()
         file.close()
+        logger.debug("Auth secret file was read.")
 
         connection = psycopg2.connect(host=host, dbname=auth_db_name, user=db_user, password=db_pass)
         cursor = connection.cursor()
+        logger.debug("Connection with database established.")
 
         query = f"SELECT * FROM blacklist WHERE \"token\" = \'{token}\'"
 
         cursor.execute(query)
+        logger.debug("Select from blacklist query executed.")
 
         records = cursor.fetchall()
 
         if cursor.rowcount == 0:    
             try:
                 decoded_token = jwt.decode(token, auth_secret, algorithms=['HS256'])
+                logger.debug(f"Token {token} has been decoded.")
                 return decoded_token
             except (Exception) as error:
+                logger.error("Provided token could not be decoded.")
                 return None
         else:
+            logger.error("Provided token is expired.")
             return None
     
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        logger.debug(error)
         if connection is not None:
             cursor.close()
             connection.close()
@@ -160,30 +193,39 @@ def validate(token):
             cursor.close()
             connection.close()
 
-def invalidate(token):
+def invalidate(token, logger):
     connection = None
     try:
         file = open(db_pass_file, "r")
         db_pass = file.read() 
         file.close()
+        logger.debug("Database password secret file was read.")
+
+        file = open(auth_secret_file, "r")
+        auth_secret = file.read()
+        file.close()
+        logger.debug("Auth secret file was read.")
 
         connection = psycopg2.connect(host=host, dbname=auth_db_name, user=db_user, password=db_pass)
         cursor = connection.cursor()
+        logger.debug("Connection with database established.")
 
         query = f"INSERT INTO blacklist(\"token\") VALUES (\'{token}\')"
 
         cursor.execute(query)
+        logger.debug(f"Token {token} added to blacklist.")
         connection.commit()
-                
-        return True
+
+        logger.info(f"Token {token} invalidated.")        
+        return jwt.decode(token, auth_secret, algorithms=['HS256']).get('clientID')
     
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        logger.debug(error)
         if connection is not None:
             cursor.close()
             connection.close()
 
-        return False
+        return None
 
     finally:
         if connection is not None:
